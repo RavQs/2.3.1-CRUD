9c97ce9ffe3379af630742fdb8ad7a13c399fd40
RavQs <ravenq1112@gmail.com>
2022-03-01 06:09:03 +0900

    properties

--- src/main/java/com/example/alishevcrud/controllers/UsersController.java
+++ src/main/java/com/example/alishevcrud/controllers/UsersController.java
@@ -2,6 +2,7 @@ package com.example.alishevcrud.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/users")
 public class UsersController {
 
-    @GetMapping
-    public String index(){
+    @GetMapping()
+    public String index(Model model) {
         return null;
     }
 
--- src/main/java/com/example/alishevcrud/model/User.java
+++ src/main/java/com/example/alishevcrud/model/User.java
@@ -1,4 +1,22 @@
 package com.example.alishevcrud.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    private Long id;
+    private String name;
+    private String post;
+    private byte age;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
